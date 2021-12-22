2e8a07e2e80b40b834880d8a9b4cca4adb8fc46a
moreno1123 <moreno.franjkovic@outlook.com>
2021-12-22 16:01:25 +0100

    init

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -4,12 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
 
 class Idea extends Model
 {
-    use HasFactory;
+
+    use HasFactory, Sluggable;
 
     protected $guarded = [];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
 
     public function user()
     {
